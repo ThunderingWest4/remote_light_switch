a7abc94eade16e0ebca81f6bf9ccdef128a76cdc
ThunderingWest4 <aaravgupta04@gmail.com>
2020-06-21 13:39:24 -0400

    Working on making the switch work, have base code and switch reading working

--- toPi/main.py
+++ toPi/main.py
@@ -1,14 +1,28 @@
-#import RPi.GPIO
+import RPi.GPIO as gpio
 import requests
 #MAKE SURE THAT THERE IS A cred.txt WITH DEVICE CREDENTIALS IN DIRECTORY
 
-f = open("cred.txt", "r")
+#resistor set up so that when the switch is in the "off" position, it reads "on"
+
+f = open("/home/pi/remote_light_switch/toPi/cred.txt", "r")
 creds = f.read().split(" ")
 f.close()
 function_name = ""
 
+swout = 12
+swin=6
+gpio.setmode(gpio.BCM)
+gpio.setup(swout, gpio.OUT)
+gpio.setup(swin, gpio.IN)
+gpio.output(swout, gpio.HIGH)
+
 URL = "https://api.particle.io/v1/devices/" + creds[0] + "/" + function_name
 
+
+prevstate = 3
 while True:
-    if("Switch is on" == True):
-        requests.post(URL, data={'args':"go", 'access_token':creds[1]})
+    if((not gpio.input(swin)) and (gpio.input(swin) != prevstate)):
+        #requests.post(URL, data={'args':"go", 'access_token':creds[1]})
+        print("flip on")
+    prevstate = gpio.input(swin)
+    #print(prevstate, gpio.input(swin))
